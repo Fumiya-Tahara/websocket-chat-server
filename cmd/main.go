683538645cd7aabadba9d705488805bc8bb01ec5
Fumiya Tahara <taharafumiya0219@gmail.com>
2024-07-24 08:53:36 +0000

    Fix: Makefile

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	err := r.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
